b8961a52bbd4adb945bb121e0ee6fcce931aa68a
Austin-Chandler <59617101+Austin-Chandler@users.noreply.github.com>
2021-12-15 14:16:03 -0700

    stufz

---
+++ Realistic-Real-Time-Grass-Rendering-With-Unity-master/Assets/Script/MoveCamera.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    //control vars
+    float mainSpeed = 100.0f;
+    float camSens = 0.25f;
+    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+    private float totalRun = 1.0f;
+
+    /// <summary>
+    /// Update function to move the camera
+    /// </summary>
+    void Update()
+    {
+        lastMouse = Input.mousePosition - lastMouse;
+        lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
+        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+        transform.eulerAngles = lastMouse;
+        lastMouse = Input.mousePosition;
+
+        //Keyboard commands
+        float f = 0.0f;
+        Vector3 p = GetBaseInput();
+        if (p.sqrMagnitude > 0)
+        { // only move while a direction key is pressed
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+            p = p * mainSpeed;
+            p = p * Time.deltaTime;
+            transform.Translate(p);
+        }
+    }
+
+    /// <summary>
+    /// Takes the key input and converts it into a vector for motion
+    /// </summary>
+    /// <returns>Vector3</returns>
+    private Vector3 GetBaseInput()
+    { //returns the basic values, if it's 0 than it's not active.
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetKey(KeyCode.W))
+        {
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            p_Velocity += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            p_Velocity += new Vector3(0, -1, 0);
+        }
+        return p_Velocity;
+    }
+}
